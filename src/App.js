64f83fb951a39e657a714eb3b4b779fd4493cc41
Denis Lemann <87332707+Front1ine@users.noreply.github.com>
2022-05-20 21:35:11 +0300

    Workfront1ine (#6)
    * main create
    
    * testcommit
    
    Co-authored-by: Vlad Ostapuk <Sp0ngbik@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">hello i'm working</div>;
+  return (
+    <div className="App">
+      hello i'm worsking hello i'm worsking hello i'm worsking
+    </div>
+  );
 }
 
 export default App;
