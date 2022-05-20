65857f97bcad4c29a22b31d1199b214bd1d3d527
Denis Lemann <denis.lemann7@gmail.com>
2022-05-20 22:23:34 +0300

    testcommitfront1ine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import "./App.css";
 
 function App() {
-
-  return <div className="App">hello this is space</div>;
-
+  return <div className="App">hello this is front1ine_space</div>;
 }
 
 export default App;
