aeed9e084a13a06f86e03bbcb2173c3dea9f18d7
Vlad Ostapuk <Sp0ngbik@gmail.com>
2022-05-20 21:55:03 +0300

    space check

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">hello this is spong space</div>;
+  return <div className="App">hello this is space</div>;
 }
 
 export default App;
