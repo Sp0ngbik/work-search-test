e6f497f046e93c6db872045849d6d3b0dc1fe1f8
Vlad Ostapuk <Sp0ngbik@gmail.com>
2022-05-25 22:32:36 +0300

    react change

---
+++ src/components/mainPage/MainPageConfig.js
@@ -0,0 +1,18 @@
+import bucketImg from "./img/basketMainPage.svg";
+import checkBoxItem from "./img/checkboxIconMainPage.svg";
+import itemPhone from "./img/iconPhone.svg";
+import incOneBlock2SecPage from "./img/img1Block2SecondPage.png";
+import incTwoBlock2SecPage from "./img/img2Block2SecondPage.png";
+import backgroundMainPage from "./img/imgMainPage.png";
+import timeIconMainPage from "./img/timeIconMainPage.svg";
+import purseItemMainPage from "./img/purseIconMainPage.svg";
+export const MainPageConfig = {
+  bucketImg,
+  checkBoxItem,
+  itemPhone,
+  incOneBlock2SecPage,
+  incTwoBlock2SecPage,
+  backgroundMainPage,
+  timeIconMainPage,
+  purseItemMainPage,
+};
