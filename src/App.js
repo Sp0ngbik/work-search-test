570921a97638098d5794262bd4748182d877ce15
Vlad <94195245+Sp0ngbik@users.noreply.github.com>
2022-05-27 15:03:31 +0300

    Main (#14)
    * prices
    
    * cost block

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
-import MainPage from "./components/mainPage/MainPage";
+// import MainPage from "./components/mainPage/MainPage";
+import ServicesPrices from "./components/servicesPrices/ServicesPrices";
 
 function App() {
 
   return (
     <div>
-      <MainPage />
+      {/* <MainPage /> */}
+      <ServicesPrices />
     </div>
   );
 
