631bd2b2c51d190a117685460370152fe57d6f60
Vlad <94195245+Sp0ngbik@users.noreply.github.com>
2022-05-27 15:29:47 +0300

    Work space (#15)
    * testcommitfront1ine
    
    * add Simple Design
    
    * react change
    
    * Spong space (#10)
    
    * sp0ng create
    
    * space check
    
    * s
    
    * Main (#14)
    
    * prices
    
    * cost block
    
    Co-authored-by: Denis Lemann <denis.lemann7@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+
+import ServicesPrices from "./components/servicesPrices/ServicesPrices";
 import MainPage from "./components/mainPage/MainPage";
 
 function App() {
 
   return (
     <div>
+      <ServicesPrices />
       <MainPage />
     </div>
   );
