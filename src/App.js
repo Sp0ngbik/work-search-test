a882f61626055162226b26f224b8d113e460440d
Vlad Ostapuk <Sp0ngbik@gmail.com>
2022-05-27 16:07:40 +0300

    master fix

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import "./App.css";
-import ServicesPrices from "./components/servicesPrices/ServicesPrices";
-
 
 // import { Route, Routes } from "react-router-dom";
 import InfoRoadMap from "./components/infoRoadMap/InfoRoadMap";
 
-import ServicesPrices from "./components/servicesPrices/ServicesPrices";
-import MainPage from "./components/mainPage/MainPage";
-
-import ServicesPrices from "./components/servicesPrices/ServicesPrices";
-import MainPage from "./components/mainPage/MainPage";
-
 function App() {
-
-
-  return (
-    <div>
-      <ServicesPrices />
-      <MainPage />
-    </div>
-  );
-
-
   return (
     <div>
       <InfoRoadMap />
     </div>
   );
-
 }
 
 export default App;
