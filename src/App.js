0dd27c2644e267b8085d1de90a1f46818fed5260
Denis Lemann <87332707+Front1ine@users.noreply.github.com>
2022-05-27 16:04:18 +0300

    Front1ine work (#18)
    * Work space (#13)
    
    * testcommitfront1ine
    
    * add Simple Design
    
    * react change
    
    * Spong space (#10)
    
    * sp0ng create
    
    * space check
    
    * s
    
    Co-authored-by: Denis Lemann <denis.lemann7@gmail.com>
    
    * Work space (#15)
    
    * testcommitfront1ine
    
    * add Simple Design
    
    * react change
    
    * Spong space (#10)
    
    * sp0ng create
    
    * space check
    
    * s
    
    * Main (#14)
    
    * prices
    
    * cost block
    
    Co-authored-by: Denis Lemann <denis.lemann7@gmail.com>
    
    * json
    
    * test
    
    Co-authored-by: Vlad <94195245+Sp0ngbik@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -5,12 +5,26 @@ import ServicesPrices from "./components/servicesPrices/ServicesPrices";
 // import { Route, Routes } from "react-router-dom";
 import InfoRoadMap from "./components/infoRoadMap/InfoRoadMap";
 
+import ServicesPrices from "./components/servicesPrices/ServicesPrices";
+import MainPage from "./components/mainPage/MainPage";
+
 function App() {
+<<<<<<< HEAD
+
+  return (
+    <div>
+      <ServicesPrices />
+      <MainPage />
+    </div>
+  );
+
+=======
   return (
     <div>
       <InfoRoadMap />
     </div>
   );
+>>>>>>> 111087988580d5bea88512144d8027ab74a22e0f
 }
 
 export default App;
