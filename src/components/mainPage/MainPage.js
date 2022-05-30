fd06fb8b7c401aa0fb32dcd117bb50c80cd7c72f
Vlad Ostapuk <Sp0ngbik@gmail.com>
2022-05-30 22:42:40 +0300

    master fixes

--- src/components/mainPage/MainPage.js
+++ src/components/mainPage/MainPage.js
@@ -3,32 +3,6 @@ import { MainPageConfig } from "./MainPageConfig";
 const MainPage = () => {
   return (
     <div>
-
-      <header className={style.headerMainPage}>
-        <div>
-          <div className={style.logoh5}>
-            Simple <span className={style.logoTextDecoration}>Design</span>
-          </div>
-          <div className={style.logoh6}>дизайн интерьера</div>
-        </div>
-        <nav className={style.navHeader}>
-          <a href="/">Главная</a>
-          <a href="/">Тест</a>
-          <a href="/">Каталог</a>
-          <a href="/">О нас</a>
-          <a href="/">Цены</a>
-        </nav>
-        <div className={style.block3Header}>
-          <div>
-            <img src={MainPageConfig.itemPhone} alt="phone" />
-          </div>
-          <div>
-            <div className={style.phoneNumber}>+7 700 505 3000</div>
-            <div className={style.dateWord}>с 9:00 до 18:00</div>
-          </div>
-        </div>
-      </header>
-
       <main>
         <div className={style.pageMain}>
           <div className={style.scaleCardMainPage}>
